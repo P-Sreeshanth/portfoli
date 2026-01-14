2b866a7e56b898fc34a6286217dbc58f0463bd22
P-Sreeshanth <peddisreeshanth18@gmail.com>
2026-01-14 21:02:37 +0530

    Remove static export config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
-  basePath: "/portfoli",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
